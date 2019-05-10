3cd3bce3cd9d52f5468faed4117999be5923c0f2
mirkhala <mirkhala@gmail.com>
2019-05-10 13:05:07 -0500

    Update ocp-new-project.sh

--- scripts/ocp-new-project.sh
+++ scripts/ocp-new-project.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 oc new-project workshopper
-oc new-app quay.io/osevg/workshopper:0.3 --name=testdrive -e WORKSHOPS_URLS="https://raw.githubusercontent.com/bmachado/amm-rehost-workshopper/master/_workshops/rehost.yml" -e JAVA_APP=false
+oc new-app quay.io/osevg/workshopper:0.3 --name=testdrive -e WORKSHOPS_URLS="https://raw.githubusercontent.com/mirkhala/amm-rehost-workshopper/master/_workshops/rehost.yml" -e JAVA_APP=false
 oc expose svc/testdrive
